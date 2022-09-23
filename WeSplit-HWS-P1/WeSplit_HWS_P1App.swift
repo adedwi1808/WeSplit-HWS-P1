69722839920d54a39926c6b1c045de9eabc126a8
adedwi1808 <adedwip1808@gmail.com>
2022-09-23 20:22:06 +0700

    Initial Commit

---
+++ WeSplit-HWS-P1/WeSplit_HWS_P1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_HWS_P1App.swift
+//  WeSplit-HWS-P1
+//
+//  Created by Ade Dwi Prayitno on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_HWS_P1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
