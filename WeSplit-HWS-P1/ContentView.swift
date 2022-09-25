938e4c97e4e8c362e762953c03570716dcc21901
adedwi1808 <adedwip1808@gmail.com>
2022-09-25 21:15:57 +0700

    ADD: Menambahkan State(checkAmount, numberOfPeople, tipPercentage) dan Array(tipPercentages)

--- WeSplit-HWS-P1/ContentView.swift
+++ WeSplit-HWS-P1/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
